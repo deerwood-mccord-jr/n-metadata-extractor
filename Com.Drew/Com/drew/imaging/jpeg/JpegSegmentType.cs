7b695058c70dde54c1411dc1fd06b0d95c5c5266
ydanila <yakodani@gmail.com>
2015-04-02 16:34:30 +0300

    JpegSegmentType enum constants fix

--- Com.Drew/Com/drew/imaging/jpeg/JpegSegmentType.cs
+++ Com.Drew/Com/drew/imaging/jpeg/JpegSegmentType.cs
@@ -142,7 +142,7 @@ namespace Com.Drew.Imaging.Jpeg
 			//    /** Start-of-Frame (12) segment identifier. */
 			//    SOF12((byte)0xCC, true),
 			IList<Com.Drew.Imaging.Jpeg.JpegSegmentType> segmentTypes = new AList<Com.Drew.Imaging.Jpeg.JpegSegmentType>();
-			foreach (Com.Drew.Imaging.Jpeg.JpegSegmentType segmentType in typeof(Com.Drew.Imaging.Jpeg.JpegSegmentType).GetEnumConstants())
+            foreach (Com.Drew.Imaging.Jpeg.JpegSegmentType segmentType in typeof(Com.Drew.Imaging.Jpeg.JpegSegmentType).GetEnumConstants<JpegSegmentType>())
 			{
 				if (segmentType.canContainMetadata)
 				{
@@ -165,7 +165,7 @@ namespace Com.Drew.Imaging.Jpeg
 		[CanBeNull]
 		public static Com.Drew.Imaging.Jpeg.JpegSegmentType FromByte(sbyte segmentTypeByte)
 		{
-			foreach (Com.Drew.Imaging.Jpeg.JpegSegmentType segmentType in typeof(Com.Drew.Imaging.Jpeg.JpegSegmentType).GetEnumConstants())
+            foreach (Com.Drew.Imaging.Jpeg.JpegSegmentType segmentType in typeof(Com.Drew.Imaging.Jpeg.JpegSegmentType).GetEnumConstants<JpegSegmentType>())
 			{
 				if (segmentType.byteValue == segmentTypeByte)
 				{
