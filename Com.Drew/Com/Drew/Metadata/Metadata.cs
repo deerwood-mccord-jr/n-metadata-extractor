880c8c26989a2250bf85f572258aef8aee78827d
ydanila <yakodani@gmail.com>
2015-04-02 10:58:45 +0300

    Metadata generics calls fixes

--- Com.Drew/Com/Drew/Metadata/Metadata.cs
+++ Com.Drew/Com/Drew/Metadata/Metadata.cs
@@ -81,7 +81,7 @@ namespace Com.Drew.Metadata
 		public void AddDirectory<T>([NotNull] T directory)
 			where T : Com.Drew.Metadata.Directory
 		{
-			GetOrCreateDirectoryList(directory.GetType()).Add(directory);
+			GetOrCreateDirectoryList<T>().Add(directory);
 		}
 
 		/// <summary>
@@ -152,10 +152,8 @@ namespace Com.Drew.Metadata
 		}
 
 		[CanBeNull]
-		private ICollection<Com.Drew.Metadata.Directory> GetDirectoryList<T>()
-			where T : Com.Drew.Metadata.Directory
+		private ICollection<Com.Drew.Metadata.Directory> GetDirectoryList(Type type)
 		{
-			System.Type type = typeof(T);
 			return _directoryListByClass.Get(type);
 		}
 
